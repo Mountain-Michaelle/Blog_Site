28c0110fee514d63a7a3641bd3863f47e0401b8a
Mountain_Michelle <michaelchinemelu24@gmail.com>
2022-06-23 04:51:39 +0100

    requirement updated48

--- images/models.py
+++ images/models.py
@@ -1,4 +1,4 @@
-from tkinter import Image
+
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify 
@@ -19,7 +19,7 @@ class Images(models.Model):
     #Overriding the save method 
         if not self.slug: 
             self.slug = slugify(self.title)
-        super(Image, self).save(*args, **kwargs)
+        super(None, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
